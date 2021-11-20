231c54b2e4d4662624b293fd69449968f6036166
Adelemphii <demilitioowner@gmail.com>
2021-11-20 17:06:55 -0600

    Simply built different.

--- DiscordBI-Rework/src/main/java/me/adelemphii/discordbirework/server/events/ServerListChangeEvent.java
+++ DiscordBI-Rework/src/main/java/me/adelemphii/discordbirework/server/events/ServerListChangeEvent.java
@@ -19,8 +19,6 @@ public class ServerListChangeEvent implements Listener {
     public void serverListEvent(ServerListPingEvent event) {
         if(plugin.getConfig().getString("motd") != null) {
             event.setMotd(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(plugin.getConfig().getString("motd"))));
-            return;
         }
-        event.setMotd(ChatColor.translateAlternateColorCodes('&', "&c&oA Pioneer's Playground; A 1.18 SMP"));
     }
 }
